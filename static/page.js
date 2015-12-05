955aa63d011dfa074fb2c5148007bf8e14013c26
xuexb <fe.xiaowu@gmail.com>
2015-12-05 16:49:30 +0800

    修复pjax标题

--- static/page.js
+++ static/page.js
@@ -33,7 +33,7 @@ $(function () {
         show: '', // 展现的动画，支持默认和fade, 可以自定义动画方式，这里为自定义的function即可。
         cache: false, // 是否使用缓存
         storage: false, // 是否使用本地存储
-        titleSuffix: ' - <%=options.name%>', // 标题后缀
+        titleSuffix: ' - ' + window.options.name, // 标题后缀
         filter: function () {},
         callback: function () {
             var url = location.href.replace(location.origin, '');
