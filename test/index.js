6212895663bb2dd8f98d4c329f5c0ea724341e4b
xuexb <fe.xiaowu@gmail.com>
2016-04-19 10:48:24 +0800

    update: 添加测试代码

---
+++ test/index.js
@@ -0,0 +1,13 @@
+/**
+ * @file 测试用例
+ * @author xiaowu
+ * @email fe.xiaowu@gmail.com
+ */
+
+import should from 'should';
+
+describe('mdjs', () => {
+    it('test', () => {
+        true.should.be.true();
+    });
+});
